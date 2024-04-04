dae7ca0e2df11b59e660fc7516a9d2049ed58ad6
Tanish Pandey <tanishpandey14@gmail.com>
2024-04-04 20:17:49 +0530

    food delivery app using react

---
+++ src/Components/Food types/foodTypesApi.jsx
@@ -0,0 +1,52 @@
+const foodTypesApi = [
+    {
+        id : 1,
+        name : "Biryani" ,
+        img : "" 
+    },
+    {
+        id : 2,
+        name : "Pizza" ,
+        img : "" 
+    },
+    {
+        id : 3,
+        name : "Cake" ,
+        img : "" 
+    },
+    {
+        id : 4,
+        name : "Thali" ,
+        img : "" 
+    },
+    {
+        id : 5,
+        name : "Burger" ,
+        img : "" 
+    },
+    {
+        id : 6,
+        name : "Paneer" ,
+        img : "" 
+    },
+    {
+        id : 7,
+        name : "North Indian" ,
+        img : "" 
+    },
+    {
+        id : 8,
+        name : "South Indian" ,
+        img : "" 
+    },
+    {
+        id : 9,
+        name : "Momos" ,
+        img : "" 
+    },
+    {
+        id : 10,
+        name : "Dosa" ,
+        img : "" 
+    },
+]
